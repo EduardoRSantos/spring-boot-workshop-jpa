fd235c475ef336f392e0577ca2fd8733dd0e806b
EduardoRSantos <eduardoow6@gmail.com>
2023-07-15 20:09:23 -0300

    deletar usuario

--- src/main/java/com/devsuperior/course/resources/UserResource.java
+++ src/main/java/com/devsuperior/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,17 +32,23 @@ public class UserResource {
     }
 
     @GetMapping("/{Id}")
-    public ResponseEntity<User> findById(@PathVariable Long Id){
+    public ResponseEntity<User> findById(@PathVariable Long Id) {
         User obj = services.findById(Id);
         return ResponseEntity.ok().body(obj);
     }
 
     // Padrao para inserção
     @PostMapping("/insert")
-    public ResponseEntity<User> insert(@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = services.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
-
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        services.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
